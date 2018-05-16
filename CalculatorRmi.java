b016c1e23e110cc25275c293bb9a139e6e6941ab
Golam Morshed <gm.morshed.99@gmail.com>
2018-05-16 23:04:04 +0600

    Add files via upload
    This is java rmi project. one is client and another is server. Any ine can use the calculator idf the server is running.

---
+++ CalculatorRmi.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculatorrmi;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+/**
+ *
+ * @author coldwarrior
+ */
+public class CalculatorRmi extends UnicastRemoteObject implements CalculatorInterface{
+    public CalculatorRmi() throws RemoteException 
+    {
+        int a,b;
+    }
+    public int add(int a , int b)throws RemoteException
+    {
+        return a+b;
+    }
+    public int sub(int a , int b)throws RemoteException
+    {
+        return a-b;
+    }
+    public int mul(int a , int b)throws RemoteException
+    { 
+        return a*b;
+    }
+    public int div(int a , int b)throws RemoteException
+    {
+        return a/b;
+    }
+}
